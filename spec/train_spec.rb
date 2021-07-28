6abe46d001be49735ec3aa7ac77f9091be61a633
Andrew Giang <giang184@gmail.com>
2021-07-28 11:14:44 -0700

    pass tests for save and clear
    co-authored-by: Adrian Camacho <adriancamacho18@gmail.com>

--- spec/train_spec.rb
+++ spec/train_spec.rb
@@ -8,4 +8,25 @@ describe '#Train' do
     end
   end
 
+  describe('#save') do
+    it("saves two trains") do
+      train1 = Train.new({:name => "Choo Choo", :id => nil})
+      train1.save()
+      train2 = Train.new({:name => "Blue", :id => nil})
+      train2.save()
+      expect(Train.all).to(eq([train1, train2]))
+    end
+  end
+
+  describe('.clear') do
+    it("clears all trains") do
+      train1 = Train.new({:name => "Choo Choo", :id => nil})
+      train1.save()
+      train2 = Train.new({:name => "Blue", :id => nil})
+      train2.save()
+      Train.clear
+      expect(Train.all).to(eq([]))
+    end
+  end
+  
 end
