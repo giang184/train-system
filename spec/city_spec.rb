b60e36ae452d59ea021cd71fe731cc3cd04b3826
Andrew Giang <giang184@gmail.com>
2021-07-28 11:42:24 -0700

    pass test 4 more tests for city
    co-authored-by: Adrian Camacho <adriancamacho18@gmail.com>

--- spec/city_spec.rb
+++ spec/city_spec.rb
@@ -53,5 +53,43 @@ describe '#City' do
       expect(City.find(city1.id)).to(eq(city1))
     end
   end
+  describe('#update') do
+    it("updates a city by id") do
+      city1 = City.new({:name => "Portland", :train_id => @train.id, :id => nil})
+      city1.save()
+      city1.update("Seattle", @train.id)
+      expect(city1.name).to(eq("Seattle"))
+    end
+  end
 
+  describe('#delete') do
+    it("deletes a city by id") do
+      city1 = City.new({:name => "Portland", :train_id => @train.id, :id => nil})
+      city1.save()
+      city2 = City.new({:name => "LA", :train_id => @train.id, :id => nil})
+      city2.save()
+      city1.delete()
+      expect(City.all).to(eq([city2]))
+    end
+  end
+
+  describe('.find_by_train') do
+    it("finds cities for a train") do
+      train2 = Train.new({:name => "Hoo Hoo", :id => nil})
+      train2.save
+      city1 = City.new({:name => "Portland", :train_id => @train.id, :id => nil})
+      city1.save()
+      city2 = City.new({:name => "LA", :train_id => train2.id, :id => nil})
+      city2.save()
+      expect(City.find_by_train(train2.id)).to(eq([city2]))
+    end
+  end
+
+  describe('#train') do
+    it("finds the train a city belongs to") do
+      city = City.new({:name => "LA", :train_id => @train.id, :id => nil})
+      city.save()
+      expect(city.train()).to(eq(@train))
+    end
+  end
 end
