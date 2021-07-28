5ced0472a2953458faf2bd562f1d4cfcebaa4371
Andrew Giang <giang184@gmail.com>
2021-07-28 11:16:41 -0700

    tests pass for find and equal
    co-authored-by: Adrian Camacho <adriancamacho18@gmail.com>

--- spec/train_spec.rb
+++ spec/train_spec.rb
@@ -28,5 +28,25 @@ describe '#Train' do
       expect(Train.all).to(eq([]))
     end
   end
+
+  describe('#==') do
+    it("is the same train if it has the same attributes as another train") do
+      train1 = Train.new({:name => "Choo Choo", :id => nil})
+      train1.save()
+      train2 = Train.new({:name => "Choo Choo", :id => nil})
+      train2.save()
+      expect(train1).to(eq(train2))
+    end
+  end
+
+  describe('.find') do
+    it("finds an a train by id") do
+      train1 = Train.new({:name => "Choo Choo", :id => nil})
+      train1.save()
+      train2 = Train.new({:name => "Blue", :id => nil})
+      train2.save()
+      expect(Train.find(train1.id)).to(eq(train1))
+    end
+  end
   
 end
